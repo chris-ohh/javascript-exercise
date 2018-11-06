4fcfcb95da4792af9a4cb1d735490b8671d7bb7b
zhime <oh2@hawaii.edu>
2018-11-05 16:48:11 -1000

    full mock exercise

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 window.onload = function () {
+  var loadClick = function () {
+    clickedValue = 0;
+
+    var list = document.createElement('ul');
+
+    for(var i = 1; i <= 100; i++) {
+      var item = document.createElement('li');
+      item.value = i;
+      item.addEventListener('click', itemClick, false);
+      item.appendChild(document.createTextNode(item.value));
+      list.appendChild(item);
+    }
+
+    document.body.insertBefore(list, okayButton);
+  };
+
+  var itemClick = function ( event ) {
+    clickedValue = event.target.value;
+
+    console.log('you clicked ' + clickedValue);
+  };
+
   var okayClick = function () {
-    console.log('okay clicked');
+    switch (clickedValue) {
+      case -1: {
+        console.log('click load');
+        break;
+      }
+
+      case 0: {
+        console.log('click a list item');
+      }
+
+      default: {
+        console.log( clickedValue + ' loaded');
+        break;
+      }
+    }
   };
 
-  var itemClick = function () {
-    console.log('list item clicked');
-
-  };
-
-  var list = document.querySelector(".list");
+  var loadButton = document.querySelector(".load");
   var okayButton = document.querySelector(".okay");
-  okayButton.onclick = okayClick;
+  var clickedValue = -1;
 
-  for(var i = 1; i <= 100; i++) {
-    var item = document.createElement('li');
-    item.onclick = itemClick;
-    item.appendChild(document.createTextNode(i));
-    list.appendChild(item);
-  }
+  loadButton.addEventListener('click', loadClick, false);
+  okayButton.addEventListener('click', okayClick, false);
+
 };
